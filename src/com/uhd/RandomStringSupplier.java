b816c3225e9065d60d1c254c3f1309c7c0eed27b
JSextonn <jsexton11295@gmail.com>
2018-10-26 14:06:23 -0500

    Editted docs

--- src/com/uhd/RandomStringSupplier.java
+++ src/com/uhd/RandomStringSupplier.java
@@ -3,6 +3,11 @@ package com.uhd;
 import java.util.Random;
 import java.util.function.Supplier;
 
+/**
+ * Supplies random strings of a given length
+ * Currently the charset is hard coded to 32-127
+ * @author Justin Sexton
+ */
 public class RandomStringSupplier implements Supplier<String> {
     private int stringLength;
     private Random random;
